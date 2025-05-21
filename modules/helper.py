652ef53d4bd6cf0d1e7dafadaff90d0e729762b9
rickert156 <rickert156@rambler.ru>
2025-05-21 12:37:08 +0400

    update helper

--- modules/helper.py
+++ modules/helper.py
@@ -3,6 +3,10 @@ def helper():
     --domain            Запуск скрипта с параметрами [domain] [type]
                         domain или email можно передать вторым параметром,
                         последним параметром передаем тип записи. Пример:
-                        python3 --domain my@email.com MX
+                        python3 SinEmail.py --domain my@email.com MX
+
+    --calling           Запуск скрипта с параметром [file_name]
+                        Поддерживается обработка CSV. Пример:
+                        python3 SinEmail.py --calling test.csv
     """
     return info
